eab0bf9229a212e413782fd4dade08228ae6e546
pebruwantoro <pebruwantoro.doni@gmail.com>
2025-07-15 20:57:18 +0700

    features-create-users-backend

--- backend-app/src/main.js
+++ backend-app/src/main.js
@@ -1,12 +1,34 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { appConfig } from './config.js';
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+
+
+import UserRepository from './application/repositories/userRepository.js';
+import UserUsecase from './application/usecases/userUsecase.js';
+import UserController from './application/controllers/userControler.js';
+import userRoute from './application/routes/userRoute.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.APP_PORT;
+// REPOSITORIES
+const userRepository = new UserRepository();
+
+// USE-CASES
+const userUsecase = new UserUsecase(userRepository);
+
+// CONTROLLERS
+const userController = new UserController(userUsecase);
+
+// ROUTES
+const userRouter = userRoute(userController);
+
+app.use('/v1/api/users', userRouter)
+
+const PORT = appConfig.port;
 app.listen(PORT, () => {
-    console.log(`SERVER IS RUNNING ON PORT: ${PORT}`)
+    console.log(`SERVER IS RUNNING ON PORT: ${PORT}`);
 });
