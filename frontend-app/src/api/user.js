5fda5863d6add4898a7b038559435907af3dd375
pebruwantoro <pebruwantoro.doni@gmail.com>
2025-07-16 22:54:35 +0700

    features-dashbord-product

--- frontend-app/src/api/user.js
+++ frontend-app/src/api/user.js
@@ -12,7 +12,7 @@ export const Login = async ({email, password}) => {
    });
 }
 
-export const createAccount = async ({name, email, password, role}) => {
+export const CreateAccount = async ({name, email, password, role}) => {
     return await fetch(`${import.meta.env.VITE_API_BACKEND_PATH}/users/register`, {
         method: 'POST',
         headers: {
--- frontend-app/src/main.jsx
+++ frontend-app/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/user/login.jsx'
 import Layout from './layout.jsx'
 import Register from './components/user/register.jsx';
+import ProductList from './components/product/list.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,6 +13,10 @@ createRoot(document.getElementById('root')).render(
         <Route element={<Layout />}>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          
+          <Route path="/dashboard">
+            <Route path="" element={<ProductList/>}/>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
