d064efa2a8d42501e3eb3481f4b89a9f132bc5b5
pebruwantoro <pebruwantoro.doni@gmail.com>
2025-07-17 14:12:58 +0700

    slicing-ui-quotation-list-page

--- frontend-app/src/api/quotation.js
+++ frontend-app/src/api/quotation.js
@@ -11,3 +11,18 @@ export const CreateQuotation = async (token, data) => {
         })
     })
 }
+
+export const List = async (token, params = {}) => {
+    const baseUrl = `${import.meta.env.VITE_API_BACKEND_PATH}/quotations`;
+    const query = new URLSearchParams(params).toString();
+    const url = query ? `${baseUrl}?${query}`: baseUrl;
+   
+    return await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+    })
+}
